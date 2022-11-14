8d21a208713d030efcc628eb4294a4896333cdbb
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2022-11-14 11:27:42 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,92 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import Card from './Card.svelte';
+
+	interface ProjectListing {
+		title: string;
+		path: string;
+	}
+
+	const prefix =
+		'https://raw.githubusercontent.com/M-L-D-H/Closing-The-Gap-In-Non-Latin-Script-Data/master';
+	let urls: string[] = [];
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	let entries: [string, Record<string, any>][] = [];
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	function sortTitles(a: Record<string, any>, b: Record<string, any>) {
+		const aTitle = a.project.title.toLowerCase();
+		const bTitle = b.project.title.toLowerCase();
+
+		if (aTitle < bTitle) {
+			return -1;
+		} else if (aTitle > bTitle) {
+			return 1;
+		} else return 0;
+	}
+
+	onMount(async () => {
+		const listResponse = await fetch(
+			'https://raw.githubusercontent.com/M-L-D-H/Closing-The-Gap-In-Non-Latin-Script-Data/master/PROJECTS.json'
+		);
+		const listData: Record<string, ProjectListing> = await listResponse.json();
+
+		const listEntries: [string, ProjectListing][] = Object.entries(listData);
+		for (const [id, details] of listEntries) {
+			urls = [...urls, `${prefix}${details.path}${id}.json`];
+		}
+
+		for (const url of urls) {
+			fetch(url)
+				.then((response) => response.json())
+				.then((data) => {
+					if (data.project) {
+						entries = [...entries, [url, data]];
+						entries.sort((a, b) => sortTitles(a[1], b[1]));
+					}
+				});
+		}
+	});
+</script>
+
+<div class="mx-auto mb-6 max-w-7xl text-gray-50">
+	<h1 class="mb-3 text-4xl">Closing the Gap</h1>
+	<h2 class="text-2xl">in Non-Latin Script Data</h2>
+</div>
+
+<div class="mb-6 bg-[#f29559] py-3.5">
+	<a href="/" class="mx-auto block max-w-7xl text-lg hover:underline">Projects</a>
+</div>
+
+<div class="mx-auto max-w-7xl">
+	<p class="mb-4 text-center text-lg text-gray-50">
+		<code>{entries.length}</code> entries
+	</p>
+
+	<div class="flex flex-wrap gap-6">
+		{#each entries as [url, data]}
+			<Card
+				title={data.project.title}
+				description={data.project.project_desc}
+				startDate={data.project.date[0].from ? data.project.date[0].from : 'N/A'}
+				endDate={data.project.date[0].to ? data.project.date[0].to : 'N/A'}
+				places={data.project.places}
+				languages={data.project.lang}
+				websiteLink={data.project.websites[0]}
+				jsonLink={url}
+				lastEdited={data.record_metadata.record_modified[
+					data.record_metadata.record_modified.length - 1
+				].record_modified_on
+					? data.record_metadata.record_modified[
+							data.record_metadata.record_modified.length - 1
+					  ].record_modified_on
+					: data.record_metadata.record_created_on}
+				categories={Object.entries(data.project.topic_relations)
+					.filter(([, v]) => v === true)
+					.map(([k]) => k)}
+				tags={data.project.keywords}
+			/>
+		{/each}
+	</div>
+</div>
---
+++ src/routes/Card.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+	export let title: string;
+	export let description: string;
+	export let startDate: string;
+	export let endDate: string;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	export let places: Record<string, any>[];
+	export let languages: string[];
+	export let websiteLink: string;
+	export let jsonLink: string;
+	export let lastEdited: string;
+	export let categories: string[];
+	export let tags: string[];
+</script>
+
+<div class="mb-auto max-w-sm rounded-lg bg-[#b8b08d] p-4 text-lg">
+	<h3 class="mb-2 font-bold">{title}</h3>
+
+	<p class="mb-4">{description}</p>
+
+	<p><strong>Start date:</strong> {startDate}</p>
+
+	<p class="mb-4"><strong>End date:</strong> {endDate}</p>
+
+	<p class="mb-4">
+		<strong>Locations:</strong>
+		{#each places as place, i}{i === 0 ? '' : ', '}<a
+				class="hover:underline"
+				href={place.place_name.ref}>{place.place_name.text}</a
+			>{/each}
+	</p>
+
+	<p class="mb-4">
+		<strong>Languages:</strong>
+		{#each languages as language, i}{i === 0 ? '' : ', '}<code>{language}</code>{/each}
+	</p>
+
+	<p class="mb-4">
+		<strong>Categories:</strong>
+		{#each categories as category, i}{i === 0 ? '' : ', '}<code>{category}</code>{/each}
+	</p>
+
+	<p class="mb-4">
+		<strong>Tags:</strong>
+		{#if tags.length > 0}
+			{#each tags as tag, i}{i === 0 ? '' : ', '}<code>{tag}</code>{/each}
+		{:else}
+			N/A
+		{/if}
+	</p>
+
+	<p class="mb-4">
+		<strong>Website:</strong>
+		<a class="break-words text-base hover:underline" href={websiteLink}
+			><code>{websiteLink}</code></a
+		>
+	</p>
+
+	<p class="mb-4">
+		<strong>JSON data:</strong>
+		<a class="break-words text-base hover:underline" href={jsonLink}
+			><code>{jsonLink}</code></a
+		>
+	</p>
+
+	<p><strong>Last edited:</strong> {lastEdited}</p>
+</div>
