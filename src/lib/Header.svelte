135db3417338e09a6230bfdef19861920a136fc7
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2022-11-27 22:37:35 +0100

    Underline currently selected page in header

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
 <div class="mx-auto mb-4 max-w-[76rem] px-4 text-gray-50">
 	<h1 class="mb-2.5 text-5xl"><a href="/">Closing the Gap</a></h1>
 	<h2 class="text-2xl">in Non-Latin Script Data</h2>
@@ -5,8 +9,20 @@
 
 <div class="mb-4 bg-[#f29559] py-3">
 	<div class="mx-auto flex max-w-[76rem] space-x-4 px-4 text-xl">
-		<a href="/" class="hover:underline">Projects</a>
-		<a href="/map" class="hover:underline">Map</a>
-		<a href="/about" class="hover:underline">About</a>
+		<a
+			href="/"
+			class="underline-offset-2 hover:underline"
+			class:underline={$page.route.id === '/'}>Projects</a
+		>
+		<a
+			href="/map"
+			class="underline-offset-2 hover:underline"
+			class:underline={$page.route.id === '/map'}>Map</a
+		>
+		<a
+			href="/about"
+			class="underline-offset-2 hover:underline"
+			class:underline={$page.route.id === '/about'}>About</a
+		>
 	</div>
 </div>
