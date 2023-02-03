591e23ed50e6d0189e7b12812275a914156d965a
marwaahmed <2101-marwaahmed@users.noreply.git.imp.fu-berlin.de>
2023-02-03 21:08:50 +0100

    display a randomly generated logo

---
+++ src/lib/Charts.svelte
@@ -0,0 +1,37 @@
+
+ 
+ <script>
+  let size = 100;
+
+  let colors = ['#003366', '#99CC00', '#880000', 'grey'];
+
+  function getRandomPosition(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  function getTrianglePoints(x, y, size) {
+    let topPoint = `${x},${y - size / 2}`;
+    let leftPoint = `${x - size / 2},${y + size / 2}`;
+    let rightPoint = `${x + size / 2},${y + size / 2}`;
+
+    return `${topPoint} ${leftPoint} ${rightPoint}`;
+  }
+
+  
+</script>
+<div style="width: 100px; height: 100px;margin-left:20px">
+<svg viewBox="0 0 500 500">
+  {#each Array(16).fill(0) as _, i}
+    <polygon points="{getTrianglePoints(getRandomPosition(500), getRandomPosition(500), size)}" fill="{colors[i % colors.length]}" />
+  {/each}
+  
+</svg>
+</div>
+
+<style>
+
+
+
+
+
+</style>
---
+++ src/routes/chart/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Chart from '$lib/Charts.svelte';
+</script>
+
+<svelte:head>
+	<title>Closing the Gap in Non-Latin-Script Data – Timeline</title>
+</svelte:head>
+
+<Chart />
