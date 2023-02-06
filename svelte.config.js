e6c21841c9ae197b6dfa828977f148a67f59aa98
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2023-02-06 21:41:02 +0100

    Remove unused dependency

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
 import adapterAuto from '@sveltejs/adapter-auto';
-// import adapterStatic from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
