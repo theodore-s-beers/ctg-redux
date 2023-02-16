80337d6fa10239a3499b694f8531259e0dbe64d4
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2023-02-17 00:53:28 +0100

    Continue work on contribution form

--- src/routes/contribute/+page.svelte
+++ src/routes/contribute/+page.svelte
@@ -6,6 +6,20 @@
 	<div class="space-y-4 rounded-lg bg-[#b8b08d] p-4 text-lg">
 		<h2 class="text-center text-2xl">Contribute</h2>
 
-		<p><strong>To be implemented…</strong></p>
+		<p>
+			This form is still under development, so bugs are likely to occur. If anything
+			doesn’t work as expected, please open an issue on
+			<a
+				class="font-medium hover:underline"
+				href="https://github.com/M-L-D-H/Closing-The-Gap-In-Non-Latin-Script-Data/issues"
+				target="_blank"
+				rel="noreferrer">GitHub</a
+			>. Please make sure to provide as much information as possible for better
+			reproducibility. We will continue to iterate on this form to include additional
+			features, such as validation on entry, automatically acquired coordinates, and an
+			option to load and edit existing JSON files.
+		</p>
+
+		<p><em>To be implemented…</em></p>
 	</div>
 </div>
